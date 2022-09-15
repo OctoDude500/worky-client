a5f43f95aa7c14b1aaf9c430d665423dde0fa1c7
HyperMonkey3D <luisfra.dev@gmail.com>
2022-09-15 17:56:11 -0600

    added additional form logic

--- apply.php
+++ apply.php
@@ -1,12 +1,22 @@
 <?php include "inc/header.php"; ?>
 <?php include "inc/validation.php"; ?>
-
+<?php // var_dump($name, $email_one, $email_two, $phone, $language, $workplace, $attempts, $location);?>
 <section class="section-background" id="hero">
         <div class="container hero-container">
             <div class="hero-title">
                 <h1>
                     <span>Apply</span>
-                    <?php echo $nameErr ?>
+                    <?php //echo $nameErr ?>
+                    <?php
+                    if (!empty($errors)){
+                        echo "<p>is  not empty</p>";
+                    } ?>
+                    <?php
+                        if(!$is_success) {
+                            echo "<?php <p>can´t post</p>";
+
+                        }
+                    ?>
                 </h1>
             </div>
             <div class="hero-sub">
@@ -27,21 +37,23 @@
             </div>
             <div class="how-cards">
                 <div class="card-container form-container">
-                    <form class="application_container" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" novalidate>
+                    <form class="application_container" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="application/x-www-form-urlencoded" novalidate>
                         <div class="form-div">
                             <label for="name">Name</label>
                             <input type="text" name="name" id="name" value="<?php echo $name; ?>" class="form-control">
                             <small ><?php echo $nameErr?></small>
+                            <small ><?php //echo  $_SERVER["CONTENT_TYPE"];?></small>
+
                         </div>
                         <div class="form-div">
                             <label for="email_one">Email</label>
                             <span>(You'll get your exams here)</span>
-                            <input type="text" name="email_one" id="email_one">
+                            <input type="email" name="email_one" id="email_one">
                             <small><?php echo $email_oneErr?></small>
                         </div>
                         <div class="form-div">
                             <label for="email_two">Confirm Email</label>
-                            <input type="text" name="email_two" id="email_two"> 
+                            <input type="email" name="email_two" id="email_two">
                             <small><?php echo $email_twoErr?></small>
                         </div>
                         <div class="form-div">
@@ -53,7 +65,7 @@
                             <label for="language">Preferred Language</label>
                             <span>(At least 80% proficiency and 60% to apply to the academy)</span>
                             <select name="language" id="language">
-                                <option selected>Select your language</option>
+                                <option selected disabled>Select your language</option>
                                 <option value="EN">English</option>
                                 <option value="DE">German</option>
                                 <option value="FR">French</option>
@@ -66,16 +78,16 @@
                             <label for="workplace">Choose your workplace</label>
                             <span>(Depends on availability)</span>
                             <select name="workplace" id="workplace">
-                                <option value="default" selected>Select workplace</option>
+                                <option value="default" selected disabled>Select workplace</option>
                                 <option value="home">From Home (WFH)</option>
                                 <option value="on_site">On Site</option>
                             </select>
                             <small><?php echo $workplaceErr?></small>
                          </div>
                         <div class="form-div">
-                            <label for="attempts">Have you been reffered or applied before?</label>
+                            <label for="attempts">Have you been referred or applied before?</label>
                             <select name="attempts" id="attemps">
-                                <option value="default" selected>Select attempts history</option>
+                                <option value="default" selected disabled>Select attempts history</option>
                                 <option value="first_time">This is my first time applying!</option>
                                 <option value="less_six">1 - 6 months ago (At least once)</option>
                                 <option value="more_six">More than 6 months ago (At least once)</option>
@@ -85,7 +97,7 @@
                         <div class="form-div">
                             <label for="location">Choose your preferred location</label>
                             <select name="location" id="location">
-                                <option value="default" selected>Select location</option>
+                                <option value="default" selected disabled>Select location</option>
                                 <option value="guatemala">Guatemala</option>
                                 <option value="xela">Xela</option>
                             </select>
@@ -98,5 +110,5 @@
             <div class="languages-footer"></div>
         </main>
     </section>
-
+<?php  ?>
     <?php include "inc/footer.php"; ?>
--- inc/validation.php
+++ inc/validation.php
@@ -2,76 +2,120 @@
 $name = $email_one = $email_two = $phone = $language = $workplace = $attempts = $location = "";
 $nameErr = $email_oneErr = $email_twoErr = $phoneErr = $languageErr = $workplaceErr = $attemptsErr = $locationErr = "";
 
+$errors = [];
+$values = [];
+$is_success = null;
+
 if (isset($_POST['submit'])) {
     if (empty($_POST["name"])) {
         $nameErr = "Name is required";
+        $errors[] = $nameErr;
+
     } else {
+
         $name = filter_input(
             INPUT_POST,
             "name",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $name;
     }
 
     if (empty($_POST["email_one"])) {
         $email_oneErr = "Email is required";
+        $errors[] = $email_oneErr;
     } else {
         $email_one = filter_input(
             INPUT_POST,
             "email_one",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $email_one;
+
     }
 
     if (empty($_POST["email_two"])) {
         $email_twoErr = "Email confirmation is required";
+        $errors[] = $email_twoErr;
     } else {
         $email_two = filter_input(
             INPUT_POST,
             "email_two",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $email_two;
+
     }
 
     if (empty($_POST["phone"])) {
         $phoneErr = "Phone is required";
+        $errors[] = $phoneErr;
     } else {
         $phone = filter_input(
             INPUT_POST,
             "phone",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $phone;
     }
 
     if (empty($_POST["language"])) {
         $languageErr = "Language is required";
+        $errors[] = $languageErr;
     } else {
         $language = filter_input(
             INPUT_POST,
             "language",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $language;
     }
 
     if (empty($_POST["workplace"])) {
         $workplaceErr = "Workplace is required";
+        $errors[] = $workplaceErr;
     } else {
         $workplace = filter_input(
             INPUT_POST,
             "workplace",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $workplace;
     }
 
     if (empty($_POST["attempts"])) {
         $attemptsErr = "Attempts is required";
+        $errors[] = $attemptsErr;
     } else {
         $attempts = filter_input(
             INPUT_POST,
             "attempts",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $attempts;
     }
 
     if (empty($_POST["location"])) {
         $locationErr = "Location is required";
+        $errors[] = $location;
     } else {
         $location = filter_input(
             INPUT_POST,
             "location",
             FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $values[] = $location;
     }
+
+    if(empty($errors)) {
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle,CURLOPT_URL, "http://localhost:8000/testing");
+        curl_setopt($curl_handle, CURLOPT_POST, 1);
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "name=$name&email=$email_one&email_confirm=$email_two&phone=$phone&language=$language&work=$workplace&attempts=$attempts&location=$location");
+        $res = curl_exec($curl_handle);
+        curl_close($curl_handle);
+        if ($res) {
+            echo "success message";
+            $is_success = true;
+        }
+    } else {
+        $is_success = false;
+    }
+
 }
+//echo "is errors" . var_dump($errors);
+//echo "is not errors" . var_dump($values);
+
+
